9da452b294d4e7d8afeb483950b7f171ad46929f
lloydestrada <iamlloydestrada@gmail.com>
2025-07-25 22:10:33 +0800

    add controller and update for authorcontroller

--- Mini-Library-System/src/main/java/com/lloyd/Mini_Library_System/RestController/AuthorController.java
+++ Mini-Library-System/src/main/java/com/lloyd/Mini_Library_System/RestController/AuthorController.java
@@ -1,4 +1,37 @@
 package com.lloyd.Mini_Library_System.RestController;
 
+
+import com.lloyd.Mini_Library_System.Entity.Author;
+import com.lloyd.Mini_Library_System.Services.AuthorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class AuthorController {
+
+    private AuthorService authorService;
+
+    //inject constructor
+    public AuthorController(AuthorService authorService){
+        this.authorService = authorService;
+    }
+
+    //find all authors
+    @GetMapping("/authors")
+    public List<Author> findAllAuthors(){
+        return authorService.findAllAuthors();
+    }
+
+    //find authors by id
+    @GetMapping("/authors/{id}")
+    public Author getAuthor(@PathVariable int id){
+        return authorService.findById(id);
+    }
+
+
 }
