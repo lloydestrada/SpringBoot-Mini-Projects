87d6e215b2205880f675a86fd7ce015d0e08cb47
lloydestrada <iamlloydestrada@gmail.com>
2025-07-29 17:41:49 +0800

    new project

---
+++ 1-thymeleafdemo/src/main/java/com/lloyd/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,42 @@
+package com.lloyd.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //new controller method to show form (HTML)
+    @RequestMapping("/Form")
+    public String showForm(){
+
+        return "helloworld-form";
+    }
+    //need controller method to process the html form
+    @RequestMapping("/processForm")
+    public String processForm(Model theModel){
+        theModel.addAttribute("theDate",java.time.LocalDateTime.now());
+        return "helloworld";
+    }
+
+    //controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String upperName(HttpServletRequest request, Model theModel){
+
+        //read the request parameter from html request
+        String theName = request.getParameter("username");
+
+        //convert data to uppercase
+        theName = theName.toUpperCase();
+
+        //create a message
+        String result = "Hey " + theName + "!" + " What's Up!";
+
+        //add message to the model
+        theModel.addAttribute("message", result);
+
+        return "helloworld";
+    }
+}
