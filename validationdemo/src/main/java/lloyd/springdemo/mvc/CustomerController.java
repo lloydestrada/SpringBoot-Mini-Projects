16ce7e1810de68f98079c449f529d7a73745a861
lloydestrada <iamlloydestrada@gmail.com>
2025-08-04 17:04:16 +0800

    add validation in controller class

--- validationdemo/src/main/java/lloyd/springdemo/mvc/CustomerController.java
+++ validationdemo/src/main/java/lloyd/springdemo/mvc/CustomerController.java
@@ -1,8 +1,12 @@
 package lloyd.springdemo.mvc;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -16,4 +20,18 @@ public class CustomerController {
         return "customer-form";
     }
 
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theBindingResult){
+
+        if(theBindingResult.hasErrors()){
+
+            return "customer-form";
+        }
+        else {
+
+            return  "customer-confirmation";
+        }
+
+    }
 }
