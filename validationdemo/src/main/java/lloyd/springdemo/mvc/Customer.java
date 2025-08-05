5cf9507c2653d5fef3da4bb3e4ab41f236ac936b
lloydestrada <iamlloydestrada@gmail.com>
2025-08-05 18:09:11 +0800

    refectored field

--- validationdemo/src/main/java/lloyd/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/lloyd/springdemo/mvc/Customer.java
@@ -1,10 +1,7 @@
 package lloyd.springdemo.mvc;
 
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -16,9 +13,15 @@ public class Customer {
     private String lastName = "";
 
     //add a number range
+    @NotNull(message = "is required")
     @Min(value = 0, message="must be greater than 0")
     @Max(value = 10, message="must be equal or less than 10")
-    private int freePasses;
+    private Integer freePasses;
+
+    //add a postal code field
+    @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
 
     public String getFirstName() {
         return firstName;
@@ -36,11 +39,19 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
